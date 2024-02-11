12f44fa9f1a790253848304000b4455658181e49
Charlie Dean <31698679+transmutrix@users.noreply.github.com>
2024-02-10 21:30:40 -0500

    cleanup

--- js/test.js
+++ js/test.js
@@ -4,11 +4,7 @@ import {
   sharpsToFlats
 } from "./synth.js";
 
-import {
-  setLabel,
-  setError,
-  setSelectedButton,
-} from "./util.js";
+import { setLabel, setError, setSelectedButton } from "./util.js";
 
 const gLogging = false;
 const BaseScore = 10;
@@ -16,8 +12,7 @@ const MinScore = 1;
 
 const GameState = Object.freeze({
   Init: Symbol("Init"),
-  PlayNote: Symbol("PlayNote"),
-  Guessed: Symbol("Guessed"),
+  Playing: Symbol("Playing"),
 });
 
 const GameMode = Object.freeze({
@@ -28,7 +23,6 @@ const GameMode = Object.freeze({
 
 let gMidiAccess = null;
 let gMode = GameMode.ShowAndPlay;
-let gRange = 49;
 let gNoteTable = noteTable49;
 let gGameState = GameState.Init;
 let gScore = 0;
@@ -36,75 +30,206 @@ let gPoints = 0;
 let gMaxScore = 0;
 let gPerfects = 0;
 let gQueueNext = 0;
+let gNoteIndex = 0;
+let gNote = null;
+let gNoteStarted = 0;
+let gGuessed = null;
+let gGameStartTime = 0;
 
+let lastLoop = 0
+let lastSecond = 0;
+let lastSecondFrames = 0;
+
+const gPlayingNotes = [];
 const gEffectContainer = document.getElementById("fx");
 const gEffects = [];
 
-function createFX(message) {
-  const el = document.createElement("div");
-  el.innerText = message;
-  el.classList.add("perfect");
-  gEffectContainer.appendChild(el);
-  gEffects.push({ time: Date.now(), lifetime: 2, el: el });
-}
+const btnPlay = document.getElementById("btn-play");
+const note1Display = document.getElementById("note1");
+const note2Display = document.getElementById("note2");
+const note1Speaker = document.getElementById("note1-speaker");
+const note2Speaker = document.getElementById("note2-speaker");
+const gameClock = document.getElementById("game-clock");
+const gameScore = document.getElementById("game-score");
+const gamePerfects = document.getElementById("game-perfects");
 
-setLabel("game-name", "Note Match");
+const btnModeShowAndPlay = document.getElementById("btn-mode1");
+const btnModePlayOnly = document.getElementById("btn-mode2");
+const btnModeShowOnly = document.getElementById("btn-mode3");
+const modeButtons = [btnModeShowAndPlay, btnModePlayOnly, btnModeShowOnly];
+
+const btnRange25 = document.getElementById("btn-25");
+const btnRange32 = document.getElementById("btn-32");
+const btnRange49 = document.getElementById("btn-49");
+const btnRange61 = document.getElementById("btn-61");
+const rangeButtons = [btnRange25, btnRange32, btnRange49, btnRange61];
+const noteTables = [noteTable25, noteTable32, noteTable49, noteTable61];
 
 const midiSupported = navigator.requestMIDIAccess;
 if (!midiSupported)  setError("This browser doesn't support WebMIDI!");
 
-function onNoteOn(note, velocity) {
-  // setLabel("message", "Note on: " + note);
-  // queueNoteOn(note, velocity);
-  synthNoteOn(note, velocity);
-  note2Speaker.style.opacity = "100%";
-  switch (gGameState) {
-    case GameState.PlayNote:
-    case GameState.Guessed:
-    {
-      gGuessed = null;
-      for (const check of gNoteTable) {
-        if (check.index === note) {
-          gGuessed = check;
-          break;
-        }
-      }
-      if (gGuessed) {
-        note2Display.textContent = `${gGuessed.name}${gGuessed.octave}`;
-        if (gNote === gGuessed) {
-          gScore += gPoints;
-          if (gPoints === BaseScore) {
-            gPerfects += 1;
-            createFX("✨ PERFECT! ✨");
-          } else if (gPoints >= Math.floor(BaseScore * 0.75)) {
-            createFX("Nice! 👍");
-          } else if (gPoints >= Math.floor(BaseScore * 0.45)) {
-            createFX("Ok! 😅");
-          } else {
-            createFX("😬😬😬");
-          }
-          gNote = null;
-          gGameState = GameState.Init;
-          gQueueNext = Date.now() + 2000;
-          setLabel("possible-points", "");
-        } else {
-          if (gNote.name === gGuessed.name) {
-            gPoints = Math.max(MinScore, gPoints - 1);
-          } else {
-            gPoints = Math.max(MinScore, gPoints - 2);
-          }
-          setLabel("possible-points", `Possible points: ${gPoints.toFixed(0)}`);
-        }
-      }
-    } break;
+setLabel("message", "Finding MIDI devices...");
+await navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+setLabel("message", "Click 'PLAY NOTE' or press Space ␣ to start.");
+
+btnModeShowAndPlay.onclick = (el, ev) => {
+  if (gMode !== GameMode.ShowAndPlay) {
+    const oldMode = gMode;
+    gMode = GameMode.ShowAndPlay;
+    showNotes();
+    if (oldMode === GameMode.ShowOnly) {
+      playNote();
+    }
+  }
+  setSelectedButton(btnModeShowAndPlay, modeButtons);
+};
+
+btnModePlayOnly.onclick = (el, ev) => {
+  if (gMode !== GameMode.PlayOnly) {
+    const oldMode = gMode;
+    gMode = GameMode.PlayOnly;
+    showNotes();
+    if (oldMode === GameMode.ShowOnly) {
+      playNote();
+    }
+  }
+  setSelectedButton(btnModePlayOnly, modeButtons);
+};
+
+btnModeShowOnly.onclick = (el, ev) => {
+  if (gMode !== GameMode.ShowOnly) {
+    gMode = GameMode.ShowOnly;
+    showNotes();
+  }
+  setSelectedButton(btnModeShowOnly, modeButtons);
+};
+
+rangeButtons.forEach((btn, i) => {
+  btn.onclick = (el, ev) => {
+    if (gNoteTable !== noteTables[i]) {
+      gNoteTable = noteTables[i];
+      chooseNote();
+      playNote();
+    }
+    setSelectedButton(btn, rangeButtons);
+  };
+});
+
+btnPlay.onclick = (el, ev) => {
+  if (audioCtx.state === "paused")  audioCtx.resume();
+  if (!gGameStartTime)              gGameStartTime = Date.now();
+  if (!gNote)                       chooseNote();
+  if (!gNoteStarted)                playNote();
+};
+
+document.body.onkeydown = function(e){
+  if (e.keyCode === 32) { // Space bar
+    btnPlay.click();
+    e.preventDefault();
   }
 }
 
-function onNoteOff(note) {
-  // setLabel("message", "Note off: " + note);
-  // queueNoteOff(note);
-  synthNoteOff(note);
-  note2Speaker.style.opacity = "0%";
+window.requestAnimationFrame(loop);
+
+function loop(timestamp) {
+  const dt = timestamp - lastLoop;
+  const date = Date.now();
+  const now = performance.now();
+
+  switch (gGameState) {
+    case GameState.Init: {
+      if (gQueueNext && date > gQueueNext) {
+        gQueueNext = 0;
+        btnPlay.click();
+      }
+    } break;
+    case GameState.Playing: {
+      if (gNoteStarted && now - gNoteStarted >= 1000.0) {
+        gNoteStarted = 0;
+        synthGameNoteOff(gNote.index);
+        note1Speaker.style.opacity = "0%";
+        // btnPlay.classList.remove("progress");
+      }
+    } break;
+  }
+
+  for (let i = gPlayingNotes.length-1; i >= 0; --i) {
+    if (date >= gPlayingNotes[i].time + 2000) {
+      synthGameNoteOff(gPlayingNotes[i].index);
+      note1Speaker.style.opacity = "0%";
+      gPlayingNotes.splice(i);
+    }
+  }
+
+  // game clock
+  if (gGameStartTime !== 0) {
+    const since = date - gGameStartTime;
+    // let ms = Math.floor(since % 1000).toString();
+    let seconds = Math.floor((since/1000) % 60).toString();
+    let minutes = Math.floor((since/1000/60) % 60).toString();
+    let hours = Math.floor(since/1000/60/60).toString();
+    // while (ms.length < 3)  ms = '0' + ms;
+    while (seconds.length < 2) seconds = '0' + seconds;
+    while (minutes.length < 2) minutes = '0' + minutes;
+    while (hours.length < 2) hours = '0' + hours;
+    if (hours !== "00") {
+      gameClock.textContent = `${hours}:${minutes}:${seconds}`;
+    } else {
+      gameClock.textContent = `${minutes}:${seconds}`;
+    }
+  }
+
+  // game score
+  if (gMaxScore !== 0) {
+    gameScore.textContent = `${gScore}/${gMaxScore} (${(gScore/gMaxScore * 100).toFixed(0)}%)`;
+  }
+  if (gPerfects !== 0) {
+    gamePerfects.textContent = `⭐️ ${gPerfects.toFixed(0)}`;
+  }
+
+  // effects
+  for (let i = gEffects.length-1; i >= 0; --i) {
+    if (date - gEffects[i].time >= gEffects[i].lifetime * 1000.0) {
+      gEffects[i].el.remove();
+      gEffects.splice(i);
+    }
+  }
+
+  // frame counter
+  lastSecondFrames += 1;
+  const seconds = Math.floor(timestamp/1000);
+  if (seconds !== lastSecond) {
+    lastSecond = seconds;
+    setLabel("fps-display", `FPS: ${lastSecondFrames}`);
+    lastSecondFrames = 0;
+  }
+  lastLoop = timestamp;
+  window.requestAnimationFrame(loop);
+}
+
+function onMIDISuccess(midiAccess) {
+  if (gLogging)  console.log(midiAccess);
+    // midiInputs = midiAccess.inputs;
+    // cpmst outputs = midiAccess.outputs;
+    gMidiAccess = midiAccess;
+    setLabel("message", "Done.");
+
+    for(const input of midiAccess.inputs.values()) {
+      input.onmidimessage = getMIDIMessage;
+    }
+
+    // for (const output of midiAccess.outputs.values()) {
+    //   if (gLogging)  console.log("output:", output);
+    // }
+
+    if (midiAccess.inputs.size === 0) {
+      setError("No MIDI input devices detected!<br>Please connect a MIDI device and refresh the page.");
+    }
+}
+
+function onMIDIFailure() {
+  setError("Could not access MIDI devices!");
 }
 
 function getMIDIMessage(midiMessage) {
@@ -168,132 +293,58 @@ function getMIDIMessage(midiMessage) {
   }
 }
 
-function onMIDISuccess(midiAccess) {
-  if (gLogging)  console.log(midiAccess);
-    // midiInputs = midiAccess.inputs;
-    // cpmst outputs = midiAccess.outputs;
-    gMidiAccess = midiAccess;
-    setLabel("message", "Done.");
-
-    for(const input of midiAccess.inputs.values()) {
-      input.onmidimessage = getMIDIMessage;
-    }
-
-    // for (const output of midiAccess.outputs.values()) {
-    //   if (gLogging)  console.log("output:", output);
-    // }
-
-    if (midiAccess.inputs.size === 0) {
-      setError("No MIDI input devices detected!<br>Please connect a MIDI device and refresh the page.");
-    }
+function onNoteOn(note, velocity) {
+  // setLabel("message", "Note on: " + note);
+  // queueNoteOn(note, velocity);
+  synthNoteOn(note, velocity);
+  note2Speaker.style.opacity = "100%";
+  switch (gGameState) {
+    case GameState.Playing:
+    {
+      gGuessed = null;
+      for (const check of gNoteTable) {
+        if (check.index === note) {
+          gGuessed = check;
+          break;
+        }
+      }
+      if (gGuessed) {
+        note2Display.textContent = `${gGuessed.name}${gGuessed.octave}`;
+        if (gNote === gGuessed) {
+          gScore += gPoints;
+          if (gPoints === BaseScore) {
+            gPerfects += 1;
+            createFX("✨ PERFECT! ✨");
+          } else if (gPoints >= Math.floor(BaseScore * 0.75)) {
+            createFX("Nice! 👍");
+          } else if (gPoints >= Math.floor(BaseScore * 0.45)) {
+            createFX("Ok! 😅");
+          } else {
+            createFX("😬😬😬");
+          }
+          gNote = null;
+          gGameState = GameState.Init;
+          gQueueNext = Date.now() + 2000;
+          setLabel("possible-points", "");
+        } else {
+          if (gNote.name === gGuessed.name) {
+            gPoints = Math.max(MinScore, gPoints - 1);
+          } else {
+            gPoints = Math.max(MinScore, gPoints - 2);
+          }
+          setLabel("possible-points", `Possible points: ${gPoints.toFixed(0)}`);
+        }
+      }
+    } break;
+  }
 }
 
-function onMIDIFailure() {
-  setError("Could not access MIDI devices!");
+function onNoteOff(note) {
+  // setLabel("message", "Note off: " + note);
+  // queueNoteOff(note);
+  synthNoteOff(note);
+  note2Speaker.style.opacity = "0%";
 }
-
-setLabel("message", "Finding MIDI devices...");
-await navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-
-setLabel("message", "Click 'PLAY NOTE' or press Space ␣ to start.");
-
-let gNoteIndex = 0;
-let gNote = null;
-let gNoteStarted = 0;
-let gGuessed = null;
-let gGameStartTime = 0;
-
-const btnPlay = document.getElementById("btn-play");
-const note1Display = document.getElementById("note1");
-const note2Display = document.getElementById("note2");
-const note1Speaker = document.getElementById("note1-speaker");
-const note2Speaker = document.getElementById("note2-speaker");
-const gameClock = document.getElementById("game-clock");
-const gameScore = document.getElementById("game-score");
-const gamePerfects = document.getElementById("game-perfects");
-
-const btnModeShowAndPlay = document.getElementById("btn-mode1");
-const btnModePlayOnly = document.getElementById("btn-mode2");
-const btnModeShowOnly = document.getElementById("btn-mode3");
-const modeButtons = [btnModeShowAndPlay, btnModePlayOnly, btnModeShowOnly];
-
-const btnRange25 = document.getElementById("btn-25");
-const btnRange32 = document.getElementById("btn-32");
-const btnRange49 = document.getElementById("btn-49");
-const btnRange61 = document.getElementById("btn-61");
-const rangeButtons = [btnRange25, btnRange32, btnRange49, btnRange61];
-
-btnModeShowAndPlay.onclick = (el, ev) => {
-  if (gMode !== GameMode.ShowAndPlay) {
-    const oldMode = gMode;
-    gMode = GameMode.ShowAndPlay;
-    showNotes();
-    if (oldMode === GameMode.ShowOnly) {
-      playNote();
-    }
-  }
-  setSelectedButton(btnModeShowAndPlay, modeButtons);
-};
-
-btnModePlayOnly.onclick = (el, ev) => {
-  if (gMode !== GameMode.PlayOnly) {
-    const oldMode = gMode;
-    gMode = GameMode.PlayOnly;
-    showNotes();
-    if (oldMode === GameMode.ShowOnly) {
-      playNote();
-    }
-  }
-  setSelectedButton(btnModePlayOnly, modeButtons);
-};
-
-btnModeShowOnly.onclick = (el, ev) => {
-  if (gMode !== GameMode.ShowOnly) {
-    gMode = GameMode.ShowOnly;
-    showNotes();
-  }
-  setSelectedButton(btnModeShowOnly, modeButtons);
-};
-
-btnRange25.onclick = (el, ev) => {
-  if (gRange !== 25) {
-    gRange = 25;
-    gNoteTable = noteTable25;
-    chooseNote();
-    playNote();
-  }
-  setSelectedButton(btnRange25, rangeButtons);
-};
-
-btnRange32.onclick = (el, ev) => {
-  if (gRange !== 32) {
-    gRange = 32;
-    gNoteTable = noteTable32;
-    chooseNote();
-    playNote();
-  }
-  setSelectedButton(btnRange32, rangeButtons);
-};
-
-btnRange49.onclick = (el, ev) => {
-  if (gRange !== 49) {
-    gRange = 49;
-    gNoteTable = noteTable49;
-    chooseNote();
-    playNote();
-  }
-  setSelectedButton(btnRange49, rangeButtons);
-};
-
-btnRange61.onclick = (el, ev) => {
-  if (gRange !== 61) {
-    gRange = 61;
-    gNoteTable = noteTable61;
-    chooseNote();
-    playNote();
-  }
-  setSelectedButton(btnRange61, rangeButtons);
-};
 
 function showNotes() {
   if (!gNote) {
@@ -322,7 +373,7 @@ function chooseNote() {
   gNote = gNoteTable[gNoteIndex];
   // if (gLogging)  console.log(gNoteIndex, gNote);
 
-  gGameState = GameState.PlayNote;
+  gGameState = GameState.Playing;
   gMaxScore += BaseScore;
   gPoints = BaseScore;
 
@@ -331,8 +382,6 @@ function chooseNote() {
   setLabel("message", "Replicate the note!<br>Click the button or press Space ␣ to play it again.");
   setLabel("possible-points", `Possible points: ${gPoints.toFixed(0)}`);
 }
-
-let gPlayingNotes = [];
 
 function playNote() {
   if (!gNote || gMode === GameMode.ShowOnly) {
@@ -345,102 +394,10 @@ function playNote() {
   // btnPlay.classList.add("progress");
 }
 
-btnPlay.onclick = (el, ev) => {
-  if (audioCtx.state === "paused")  audioCtx.resume();
-  if (!gGameStartTime)              gGameStartTime = Date.now();
-  if (!gNote)                       chooseNote();
-  if (!gNoteStarted)                playNote();
-};
-
-document.body.onkeydown = function(e){
-  if (e.keyCode == 32) { // Space bar
-    btnPlay.click();
-    e.preventDefault();
-  }
+function createFX(message) {
+  const el = document.createElement("div");
+  el.innerText = message;
+  el.classList.add("perfect");
+  gEffectContainer.appendChild(el);
+  gEffects.push({ time: Date.now(), lifetime: 2, el: el });
 }
-
-function updateGame(dt) {
-  const now = performance.now();
-  switch (gGameState) {
-    case GameState.Init: {
-      if (gQueueNext && Date.now() > gQueueNext) {
-        gQueueNext = 0;
-        btnPlay.click();
-      }
-    } break;
-    case GameState.PlayNote: {
-      if (gNoteStarted && now - gNoteStarted >= 1000.0) {
-        gNoteStarted = 0;
-        synthGameNoteOff(gNote.index);
-        note1Speaker.style.opacity = "0%";
-        // btnPlay.classList.remove("progress");
-      }
-    } break;
-    case GameState.Guessed: {
-    } break;
-  }
-
-  const date = Date.now();
-  for (let i = gPlayingNotes.length-1; i >= 0; --i) {
-    if (date >= gPlayingNotes[i].time + 2000) {
-      synthGameNoteOff(gPlayingNotes[i].index);
-      note1Speaker.style.opacity = "0%";
-      gPlayingNotes.splice(i);
-    }
-  }
-}
-
-let lastLoop = 0
-let lastSecond = 0;
-let lastSecondFrames = 0;
-function loop(timestamp) {
-  const dt = timestamp - lastLoop;
-  updateGame(dt);
-
-  lastSecondFrames += 1;
-  const seconds = Math.floor(timestamp/1000);
-  if (seconds !== lastSecond) {
-    lastSecond = seconds;
-    setLabel("fps-display", `FPS: ${lastSecondFrames}`);
-    lastSecondFrames = 0;
-  }
-
-  // game clock
-  if (gGameStartTime !== 0) {
-    const since = Date.now() - gGameStartTime;
-    // let ms = Math.floor(since % 1000).toString();
-    let seconds = Math.floor((since/1000) % 60).toString();
-    let minutes = Math.floor((since/1000/60) % 60).toString();
-    let hours = Math.floor(since/1000/60/60).toString();
-    // while (ms.length < 3)  ms = '0' + ms;
-    while (seconds.length < 2) seconds = '0' + seconds;
-    while (minutes.length < 2) minutes = '0' + minutes;
-    while (hours.length < 2) hours = '0' + hours;
-    if (hours !== "00") {
-      gameClock.textContent = `${hours}:${minutes}:${seconds}`;
-    } else {
-      gameClock.textContent = `${minutes}:${seconds}`;
-    }
-  }
-
-  // game score
-  if (gMaxScore !== 0) {
-    gameScore.textContent = `${gScore}/${gMaxScore} (${(gScore/gMaxScore * 100).toFixed(0)}%)`;
-  }
-  if (gPerfects !== 0) {
-    gamePerfects.textContent = `⭐️ ${gPerfects.toFixed(0)}`;
-  }
-
-  // effects
-  const now = Date.now();
-  for (let i = gEffects.length-1; i >= 0; --i) {
-    if (now - gEffects[i].time >= gEffects[i].lifetime * 1000.0) {
-      gEffects[i].el.remove();
-      gEffects.splice(i);
-    }
-  }
-
-  lastLoop = timestamp;
-  window.requestAnimationFrame(loop);
-}
-window.requestAnimationFrame(loop);
