7559152361346a8363bf86f5d368738f5bcb0192
Charlie Dean <31698679+transmutrix@users.noreply.github.com>
2024-02-10 20:42:51 -0500

    cleanup

--- js/test.js
+++ js/test.js
@@ -153,101 +153,45 @@ function getMIDIMessage(midiMessage) {
   let note = midiMessage.data[1];
   let velocity = midiMessage.data[2];
 
-  if (command === 0b10000000) { // = 80= 128	Chan 1 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10000001) { // = 81= 129	Chan 2 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10000010) { // = 82= 130	Chan 3 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10000011) { // = 83= 131	Chan 4 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10000100) { // = 84= 132	Chan 5 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10000101) { // = 85= 133	Chan 6 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10000110) { // = 86= 134	Chan 7 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10000111) { // = 87= 135	Chan 8 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10001000) { // = 88= 136	Chan 9 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10001001) { // = 89= 137	Chan 10 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10001010) { // = 8A= 138	Chan 11 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10001011) { // = 8B= 139	Chan 12 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10001100) { // = 8C= 140	Chan 13 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10001101) { // = 8D= 141	Chan 14 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10001110) { // = 8E= 142	Chan 15 Note off	Note Number (0-127)	Note Velocity (0-127)
-    onNoteOff(note);
-  }
-  if (command === 0b10001111) { // = 8F= 143	Chan 16 Note off	Note Number (0-127)	Note Velocity (0-127)
+  if (
+    command === 0b10000000 || // = 80= 128	Chan 1 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10000001 || // = 81= 129	Chan 2 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10000010 || // = 82= 130	Chan 3 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10000011 || // = 83= 131	Chan 4 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10000100 || // = 84= 132	Chan 5 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10000101 || // = 85= 133	Chan 6 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10000110 || // = 86= 134	Chan 7 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10000111 || // = 87= 135	Chan 8 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10001000 || // = 88= 136	Chan 9 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10001001 || // = 89= 137	Chan 10 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10001010 || // = 8A= 138	Chan 11 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10001011 || // = 8B= 139	Chan 12 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10001100 || // = 8C= 140	Chan 13 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10001101 || // = 8D= 141	Chan 14 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10001110 || // = 8E= 142	Chan 15 Note off	Note Number (0-127)	Note Velocity
+    command === 0b10001111    // = 8F= 143	Chan 16 Note off	Note Number (0-127)	Note Velocity
+  ) {
     onNoteOff(note);
   }
 
-  if (command === 0b10010000) { // = 90= 144	Chan 1 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10010001) { // = 91= 145	Chan 2 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10010010) { // = 92= 146	Chan 3 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10010011) { // = 93= 147	Chan 4 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10010100) { // = 94= 148	Chan 5 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10010101) { // = 95= 149	Chan 6 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10010110) { // = 96= 150	Chan 7 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10010111) { // = 97= 151	Chan 8 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10011000) { // = 98= 152	Chan 9 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10011001) { // = 99= 153	Chan 10 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10011010) { // = 9A= 154	Chan 11 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10011011) { // = 9B= 155	Chan 12 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10011100) { // = 9C= 156	Chan 13 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10011101) { // = 9D= 157	Chan 14 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10011110) { // = 9E= 158	Chan 15 Note on	Note Number (0-127)	Note Velocity (0-127)
-    velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
-  }
-  if (command === 0b10011111) { // = 9F= 159	Chan 16 Note on	Note Number (0-127)	Note Velocity (0-127)
+  if (
+    command === 0b10010000 || // = 90= 144	Chan 1 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10010001 || // = 91= 145	Chan 2 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10010010 || // = 92= 146	Chan 3 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10010011 || // = 93= 147	Chan 4 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10010100 || // = 94= 148	Chan 5 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10010101 || // = 95= 149	Chan 6 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10010110 || // = 96= 150	Chan 7 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10010111 || // = 97= 151	Chan 8 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10011000 || // = 98= 152	Chan 9 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10011001 || // = 99= 153	Chan 10 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10011010 || // = 9A= 154	Chan 11 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10011011 || // = 9B= 155	Chan 12 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10011100 || // = 9C= 156	Chan 13 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10011101 || // = 9D= 157	Chan 14 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10011110 || // = 9E= 158	Chan 15 Note on	Note Number (0-127)	Note Velocity
+    command === 0b10011111    // = 9F= 159	Chan 16 Note on	Note Number (0-127)	Note Velocity
+  ) {
     velocity > 0?  onNoteOn(note, velocity)  :  onNoteOff(note);
   }
 }
@@ -287,12 +231,6 @@ let gGuessed = null;
 let gGameStartTime = 0;
 
 setLabel("message", "Click 'PLAY NOTE' or press Space ␣ to start.");
-// document.body.addEventListener('click', (element, event) => {
-//   if (audioCtx.state === 'paused') {
-//     audioCtx.resume();
-//   }
-//   setLabel("message", "Replicate the pitch! (Click to replay)");
-// }, true);
 
 const btnPlay = document.getElementById("btn-play");
 const note1Display = document.getElementById("note1");
